8c16db385c31dcc5bdf7cc598254202c5327ac85
CppPhil <CppPhil@users.noreply.github.com>
2020-01-28 20:28:38 +0100

    Disable CAF warnings

--- src/cpp/application/main.cpp
+++ src/cpp/application/main.cpp
@@ -1,44 +1,44 @@
 #include <file.hpp> // cp::print
-
 #include <iostream>
 #include <pl/string_view.hpp> // pl::string_view
 #include <string>
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wduplicated-branches"
+#pragma GCC diagnostic ignored "-Wshadow"
+#pragma GCC diagnostic ignored "-Wredundant-decls"
 #include "caf/all.hpp"
+#pragma GCC diagnostic pop
 
-using std::endl;
-using std::string;
 
-using namespace caf;
-
-behavior mirror(event_based_actor* self) {
+caf::behavior mirror(caf::event_based_actor* self) {
   // return the (initial) actor behavior
   return {// a handler for messages containing a single string
           // that replies with a string
-          [=](const string& what) -> string {
+          [=](const std::string& what) -> std::string {
             // prints "Hello World!" via aout (thread-safe cout wrapper)
-            aout(self) << what << endl;
+            caf::aout(self) << what << std::endl;
             cp::print(what.c_str());
 
             const pl::string_view sv{what};
 
             // reply "!dlroW olleH"
-            return string(sv.rbegin(), sv.rend());
+            return std::string(sv.rbegin(), sv.rend());
           }};
 }
 
-void hello_world(event_based_actor* self, const actor& buddy) {
+void hello_world(caf::event_based_actor* self, const caf::actor& buddy) {
   // send "Hello World!" to our buddy ...
   self->request(buddy, std::chrono::seconds(10), "Hello World!")
     .then(
       // ... wait up to 10s for a response ...
-      [=](const string& what) {
+      [=](const std::string& what) {
         // ... and print it
-        aout(self) << what << endl;
+        caf::aout(self) << what << std::endl;
       });
 }
 
-void caf_main(actor_system& system) {
+void caf_main(caf::actor_system& system) {
   // create a new actor that calls 'mirror()'
   auto mirror_actor = system.spawn(mirror);
   // create another actor that calls 'hello_world(mirror_actor)';
