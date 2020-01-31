56433fcf7ce9a9b6cc0773aa3e94b7cad733e187
CppPhil <CppPhil@users.noreply.github.com>
2020-01-31 18:43:06 +0100

    math_actor type alias

--- src/cpp/application/main.cpp
+++ src/cpp/application/main.cpp
@@ -11,12 +11,13 @@ CAF_MSG_TYPE_ADD_ATOM(multiplication_atom);
 CAF_MSG_TYPE_ADD_ATOM(division_atom);
 } // namespace caf
 
-caf::typed_behavior<
+using math_actor = caf::typed_actor<
   caf::replies_to<caf::addition_atom, int, int>::with<int>,
   caf::replies_to<caf::subtraction_atom, int, int>::with<int>,
   caf::replies_to<caf::multiplication_atom, int, int>::with<int>,
-  caf::replies_to<caf::division_atom, int, int>::with<int>>
-math() {
+  caf::replies_to<caf::division_atom, int, int>::with<int>>;
+
+math_actor::behavior_type math() {
   return {[](caf::addition_atom, int x, int y) { return x + y; },
           [](caf::subtraction_atom, int x, int y) { return x - y; },
           [](caf::multiplication_atom, int x, int y) { return x * y; },
@@ -36,12 +37,7 @@ void caf_main(caf::actor_system& system) {
   // create another actor that calls 'hello_world(mirror_actor)';
   system.spawn(&cp::hello_world, mirror_actor);
 
-  caf::typed_actor<
-    caf::replies_to<caf::addition_atom, int, int>::with<int>,
-    caf::replies_to<caf::subtraction_atom, int, int>::with<int>,
-    caf::replies_to<caf::multiplication_atom, int, int>::with<int>,
-    caf::replies_to<caf::division_atom, int, int>::with<int>>
-    a{system.spawn(math)};
+  math_actor a{system.spawn(math)};
   caf::scoped_actor self{system};
   self->request(a, caf::infinite, caf::addition_atom{}, 1, 2)
     .receive([](int z) { std::cout << "1+2=" << z << '\n'; },
