946b6c25a94c12e04404cb151f24c6198c766dfb
CppPhil <CppPhil@users.noreply.github.com>
2020-01-31 17:46:02 +0100

    Atom example that seems to work

--- src/cpp/application/main.cpp
+++ src/cpp/application/main.cpp
@@ -1,7 +1,27 @@
 #include <caf/all.hpp>
+#include <caf/io/all.hpp>
 #include <file.hpp>
 #include <hello_world.hpp>
 #include <mirror.hpp>
+
+namespace caf {
+CAF_MSG_TYPE_ADD_ATOM(addition_atom);
+CAF_MSG_TYPE_ADD_ATOM(subtraction_atom);
+CAF_MSG_TYPE_ADD_ATOM(multiplication_atom);
+CAF_MSG_TYPE_ADD_ATOM(division_atom);
+} // namespace caf
+
+caf::behavior math() {
+  return {[](caf::addition_atom, int x, int y) { return x + y; },
+          [](caf::subtraction_atom, int x, int y) { return x - y; },
+          [](caf::multiplication_atom, int x, int y) { return x * y; },
+          [](caf::division_atom, int x, int y) -> caf::result<int> {
+            if (y == 0) {
+              return caf::sec::invalid_argument;
+            }
+            return x / y;
+          }};
+}
 
 void caf_main(caf::actor_system& system) {
   cp::print_version_information();
@@ -10,7 +30,14 @@ void caf_main(caf::actor_system& system) {
   auto mirror_actor = system.spawn(&cp::mirror);
   // create another actor that calls 'hello_world(mirror_actor)';
   system.spawn(&cp::hello_world, mirror_actor);
-  // system will wait until both actors are destroyed before leaving main
+
+  caf::actor a = system.spawn(math);
+  caf::scoped_actor self{system};
+  self->request(a, caf::infinite, caf::addition_atom{}, 1, 2)
+    .receive([](int z) { std::cout << "1+2=" << z << '\n'; },
+             [&system](caf::error& err) {
+               std::cout << "Error: " << system.render(err) << '\n';
+             });
 }
 
 // creates a main function for us that calls our caf_main
