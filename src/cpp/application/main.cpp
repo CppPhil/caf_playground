f4970c5d44d0ae803076c3bbd8dee6fc4644c894
CppPhil <CppPhil@users.noreply.github.com>
2020-02-01 14:21:19 +0100

    Add a buddy for the typed function based actor
    aout seems to interpret characters as decimal integers

--- src/cpp/application/main.cpp
+++ src/cpp/application/main.cpp
@@ -4,7 +4,17 @@
 #include <file.hpp>
 #include <hello_world.hpp>
 #include <mirror.hpp>
+#include <pl/current_function.hpp>
 #include <typed_calculator.hpp>
+
+void typed_cell_buddy_actor_fun(caf::event_based_actor* self,
+                                const cp::cell& buddy) {
+  self->request(buddy, caf::infinite, caf::put_atom{}, 55);
+
+  self->request(buddy, caf::infinite, caf::get_atom{}).then([self](int result) {
+    caf::aout(self) << PL_CURRENT_FUNCTION << ": " << result << "\n";
+  });
+}
 
 void caf_main(caf::actor_system& system) {
   cp::print_version_information();
@@ -22,13 +32,7 @@ void caf_main(caf::actor_system& system) {
              });
 
   auto cell = system.spawn(&cp::type_checked_cell);
-  self->request(cell, caf::infinite, caf::put_atom{}, 50);
-
-  self->request(cell, caf::infinite, caf::get_atom{})
-    .receive([](int result) { std::cout << "Here's: " << result << '\n'; },
-             [&system](caf::error& err) {
-               std::cout << "Error: " << system.render(err) << '\n';
-             });
+  auto cell_buddy = system.spawn(&typed_cell_buddy_actor_fun, cell);
 }
 
 struct foo {
